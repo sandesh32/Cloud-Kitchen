999a151ef7bb0a01631041b2b4085542bf3f72f2
roatt-tilma <roshanbhattmnr@gmail.com>
2022-02-23 23:44:57 +0545

    footer done

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,4 +1,4 @@
-import LandingPage from "./pages/LandingPage";
+import LandingPage from "./Pages/LandingPage";
 
 function App() {
   return (
--- Frontend/src/pages/LandingPage.js
+++ Frontend/src/pages/LandingPage.js
@@ -1,9 +1,11 @@
 import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 
 const LandingPage = () => {
     return ( 
         <div>
             <Navbar/>
+            <Footer/>
         </div>
      );
 }
