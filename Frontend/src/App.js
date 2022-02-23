4fdccc8be9d6396e200b42432a07839eb29925ff
Sandesh Dhungana <dhunganasandesh32@gmail.com>
2022-02-23 22:33:25 +0545

    Added react router dom

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,18 @@
-import LandingPage from "./pages/LandingPage";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import LandingPage from "./Pages/LandingPage";
 
 function App() {
   return (
-    <div>
-      <LandingPage/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage/>}/>      
+      </Routes>
+    </Router>
   );
 }
 
