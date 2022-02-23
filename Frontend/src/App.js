61c2af674ea84143436202a465f5cbe67aab5c9f
roatt-tilma <roshanbhattmnr@gmail.com>
2022-02-23 19:06:08 +0545

    reinitialized

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Cloud Kitchen 
+          Learn React
         </a>
       </header>
     </div>
