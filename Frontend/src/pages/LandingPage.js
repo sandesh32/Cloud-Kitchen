58599ab5100e5e15e89aa026a5cfd2c0e9171310
Sandesh Dhungana <dhunganasandesh32@gmail.com>
2022-03-21 23:43:41 +0545

    Authentication Management

--- Frontend/src/pages/LandingPage.js
+++ Frontend/src/pages/LandingPage.js
@@ -1,6 +1,10 @@
 import Navbar from "./Components/Navbar";
 import Board from "./Components/Board";
 import Offer from "./Components/Offer";
+import LPBanner from "./Components/LPBanner";
+import LPOurServices from "./Components/LPOurServices";
+import LPStatistics from "./Components/LPStatistics";
+import LPSubscribe from "./Components/LPSubscribe";
 
 const LandingPage = () => {
     return ( 
@@ -12,23 +16,13 @@ const LandingPage = () => {
             <Board/>
             <Board/>
             <Board/>
-        </div>
-    )
-            
-import LPBanner from "./Components/LPBanner";
-import LPOurServices from "./Components/LPOurServices";
-import LPStatistics from "./Components/LPStatistics";
-import LPSubscribe from "./Components/LPSubscribe";
-
-const LandingPage = () => {
-    return ( 
-        <div>
             <LPBanner/>
             <LPOurServices/>
             <LPStatistics/>
             <LPSubscribe/>
         </div>
-     );
-}
+    )}
+            
+
  
-// export default LandingPage;
+ export default LandingPage;
